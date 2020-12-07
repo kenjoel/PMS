5051418c784f3a443ab2e2ccbae42d76fb3fccc3
ShemBrooklyne <shemnyatti@gmail.com>
2020-12-08 02:16:25 +0300

    Intro activities

---
+++ app/src/main/java/com/moringaschool/pms/IntroActivity.java
@@ -0,0 +1,41 @@
+package com.moringaschool.pms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class IntroActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.textView1) TextView mNextBtn;
+    @BindView(R.id.textView2) TextView mSkipBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        ButterKnife.bind(this);
+
+        mNextBtn.setOnClickListener(this);
+        mSkipBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mNextBtn) {
+            Intent intent = new Intent(IntroActivity.this, infoActivity.class);
+            startActivity(intent);
+        }
+
+        if (view == mSkipBtn) {
+            Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/com/moringaschool/pms/infoActivity.java
@@ -0,0 +1,39 @@
+package com.moringaschool.pms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class infoActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.textView3) TextView mNextBtn;
+    @BindView(R.id.textView4) TextView mBackBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+        ButterKnife.bind(this);
+
+        mNextBtn.setOnClickListener(this);
+        mBackBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mNextBtn) {
+            Intent intent = new Intent(infoActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        if (v == mBackBtn) {
+            Intent intent = new Intent(infoActivity.this, IntroActivity.class);
+            startActivity(intent);
+        }
+    }
+}
