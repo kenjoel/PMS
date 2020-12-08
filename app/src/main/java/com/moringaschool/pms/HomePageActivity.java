c7e21ab172958c4fc272b7d0d040e9b1fa624e0c
ShemBrooklyne <shemnyatti@gmail.com>
2020-12-08 03:01:14 +0300

    Home page Activity

---
+++ app/src/main/java/com/moringaschool/pms/HomePageActivity.java
@@ -0,0 +1,22 @@
+package com.moringaschool.pms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+
+
+}
--- app/src/main/java/com/moringaschool/pms/MainActivity.java
+++ app/src/main/java/com/moringaschool/pms/MainActivity.java
@@ -2,14 +2,33 @@ package com.moringaschool.pms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.viewContent) Button mViewContent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mViewContent.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mViewContent) {
+            Intent intent = new Intent(MainActivity.this, HomePageActivity.class);
+            startActivity(intent);
+        }
     }
 
 }
